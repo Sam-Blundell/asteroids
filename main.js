cd8af2f2e9c5f233b24b4bf788aed37a7b3c77ef
Sam Blundell <samblundell@PB-MAC5.local>
2023-02-14 22:25:12 +0000

    create event listener to load scripts when loading is finished

---
+++ main.js
@@ -0,0 +1,7 @@
+
+window.addEventListener('load', () => {
+    const gameScreen = document.getElementById('gameScreen');
+    const context = gameScreen.getContext('2d');
+    gameScreen.height = 600;
+    gameScreen.width = 600;
+})
