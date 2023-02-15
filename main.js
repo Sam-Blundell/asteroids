b12e22796489d1993e7b7e00c19ba6b5814bee65
Sam Blundell <samblundell@PB-MAC5.local>
2023-02-15 01:39:22 +0000

    add animation and update loop to main.js and create game instance

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+import Game from './game.js';
 
 window.addEventListener('load', () => {
     const gameScreen = document.getElementById('gameScreen');
     const context = gameScreen.getContext('2d');
     gameScreen.height = 600;
     gameScreen.width = 600;
+    const game = new Game(gameScreen.height, gameScreen.width);
+
+    let lastTimeStamp = 0;
+    const animate = (timeStamp) => {
+        context.fillStyle = 'black';
+        context.fillRect(0, 0, gameScreen.height, gameScreen.width);
+        context.fillStyle = 'white';
+        const timeDelta = timeStamp - lastTimeStamp;
+        lastTimeStamp = timeStamp;
+        game.update(timeDelta);
+        game.draw(context);
+        requestAnimationFrame(animate);
+    }
+    animate(0);
 })
