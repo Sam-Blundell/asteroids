ae242432a32b67240327668902368a1774510453
Sam Blundell <samblundell@PB-MAC5.local>
2023-02-15 01:39:02 +0000

    name game class and create input handler instance on it, create update method

--- game.js
+++ game.js
@@ -1,10 +1,15 @@
+import InputHandler from './inputHandler.js';
 import SpaceShip from './spaceship.js';
 
-export default class {
+export default class Game {
     constructor(height, width) {
         this.screenHeight = height;
         this.screenWidth = width;
+        this.input = new InputHandler(this);
         this.spaceShip = new SpaceShip(this);
+    }
+    update(timeDelta) {
+        this.spaceShip.update(this.input.pressedKeys);
     }
     draw(context) {
         this.spaceShip.draw(context);
