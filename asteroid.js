bd551a4be3b1f6be1dfdfef3fbc9c7837498fb87
Sam Blundell <samblundell@PB-MAC5.local>
2023-02-15 21:52:33 +0000

    create asteroid classes

---
+++ asteroid.js
@@ -0,0 +1,30 @@
+
+class Asteroid {
+    constructor(game) {
+
+    }
+    update(timeDelta) {
+
+    }
+    draw(context) {
+        
+    }
+}
+
+export class BigAsteroid extends Asteroid {
+    constructor() {
+        super();
+    }
+}
+
+export class MediumAsteroid extends Asteroid {
+    constructor() {
+        super();
+    }
+}
+
+export class SmallAsteroid extends Asteroid {
+    constructor() {
+        super();
+    }
+}
