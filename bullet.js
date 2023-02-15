83ab59331c938d2f2596ca4e3022dd0eec11405c
Sam Blundell <samblundell@PB-MAC5.local>
2023-02-15 21:45:50 +0000

    implement bullet update and draw methods

--- bullet.js
+++ bullet.js
@@ -1,21 +1,37 @@
-
 export default class Bullet {
-    constructor(game) {
-        this.game = game;
-        this.width = 2;
-        this.height = 2;
+    constructor(spaceship) {
+        this.game = spaceship.game;
+        this.spaceship = spaceship;
+        this.size = 4;
         // get from spaceship
-        this.xPos = null;
-        this.yPos = null;
-        this.direction = null;
-        this.xVelocity = null;
-        this.yVelocity = null;
-        this.lifeTime = 1;
+        this.xPos = spaceship.xPos;
+        this.yPos = spaceship.yPos;
+        this.xVelocity = 5 * Math.cos(spaceship.direction);
+        this.yVelocity = 5 * Math.sin(spaceship.direction);
+        this.age = 0;
+        this.markedForDeletion = false;
     }
     update(timeDelta) {
+        this.age += timeDelta;
+        if (this.age > 1500) this.markedForDeletion = true;
+        this.xPos += this.xVelocity;
+        this.yPos += this.yVelocity;
 
+        // Wrap around the x-axis
+        if (this.xPos < -this.size / 2) {
+            this.xPos = this.game.screenWidth + this.size / 2;
+        } else if (this.xPos > this.game.screenWidth + this.size / 2) {
+            this.xPos = -this.size / 2;
+        }
+
+        // Wrap around the y-axis
+        if (this.yPos < -this.size / 2) {
+            this.yPos = this.game.screenHeight + this.size / 2;
+        } else if (this.yPos > this.game.screenHeight + this.size / 2) {
+            this.yPos = -this.size / 2;
+        }
     }
     draw(context) {
-
+        context.fillRect(this.xPos, this.yPos, this.size, this.size);
     }
 }
