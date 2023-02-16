5387493f0b918c5c3f5ecd0bdeb3e68ee86f9103
Sam Blundell <samblundell@PB-MAC5.local>
2023-02-16 10:21:48 +0000

    draw circular asteroid to screen and implement simple movement

--- asteroid.js
+++ asteroid.js
@@ -1,33 +1,54 @@
 class Asteroid {
     constructor(game) {
         this.game = game;
-        this.xPos = 0;
-        this.yPos = 0;
-        this.xVelocity = 0;
-        this.yVelocity = 0;
+        this.xPos = 300;
+        this.yPos = 300;
+        this.xVelocity = 1;
+        this.yVelocity = 1;
     }
     update(timeDelta) {
+        this.xPos += this.xVelocity;
+        this.yPos += this.yVelocity;
 
+        // Wrap around the x-axis
+        if (this.xPos < -this.radius / 2) {
+            this.xPos = this.game.screenWidth + this.radius / 2;
+        } else if (this.xPos > this.game.screenWidth + this.radius / 2) {
+            this.xPos = -this.radius / 2;
+        }
+
+        // Wrap around the y-axis
+        if (this.yPos < -this.radius / 2) {
+            this.yPos = this.game.screenHeight + this.radius / 2;
+        } else if (this.yPos > this.game.screenHeight + this.radius / 2) {
+            this.yPos = -this.radius / 2;
+        }
     }
     draw(context) {
-        
+        context.beginPath();
+        context.arc(this.xPos, this.yPos, this.radius, 0, Math.PI * 2, true);
+        context.strokeStyle = 'white';
+        context.stroke();
     }
 }
 
 export class BigAsteroid extends Asteroid {
-    constructor() {
-        super();
+    constructor(game) {
+        super(game);
+        this.radius = 50;
     }
 }
 
 export class MediumAsteroid extends Asteroid {
-    constructor() {
-        super();
+    constructor(game) {
+        super(game);
+        this.radius = 25;
     }
 }
 
 export class SmallAsteroid extends Asteroid {
-    constructor() {
-        super();
+    constructor(game) {
+        super(game);
+        this.radius = 10;
     }
 }
--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 import InputHandler from './inputHandler.js';
 import SpaceShip from './spaceship.js';
+import { BigAsteroid } from './asteroid.js';
 
 export default class Game {
     constructor(height, width) {
@@ -7,11 +8,14 @@ export default class Game {
         this.screenWidth = width;
         this.input = new InputHandler(this);
         this.spaceShip = new SpaceShip(this);
+        this.testAsteroid = new BigAsteroid(this);
     }
     update(timeDelta) {
         this.spaceShip.update(timeDelta, this.input.pressedKeys);
+        this.testAsteroid.update(timeDelta);
     }
     draw(context) {
         this.spaceShip.draw(context);
+        this.testAsteroid.draw(context);
     }
 }
