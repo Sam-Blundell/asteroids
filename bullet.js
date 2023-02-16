024ed91dca69a9808018702104c0c23859afc74f
Sam Blundell <samblundell@PB-MAC5.local>
2023-02-16 20:29:58 +0000

    add collision detection between bullets and asteroids

--- bullet.js
+++ bullet.js
@@ -3,13 +3,24 @@ export default class Bullet {
         this.game = spaceship.game;
         this.spaceship = spaceship;
         this.size = 4;
-        // get from spaceship
         this.xPos = spaceship.xPos;
         this.yPos = spaceship.yPos;
         this.xVelocity = 5 * Math.cos(spaceship.direction);
         this.yVelocity = 5 * Math.sin(spaceship.direction);
         this.age = 0;
         this.markedForDeletion = false;
+    }
+    checkCollision() {
+        this.game.asteroids.forEach(asteroid => {
+            const distanceX = (this.xPos + 2) - asteroid.xPos;
+            const distanceY = (this.yPos + 2) - asteroid.yPos;
+            const distance = Math.sqrt(distanceX*distanceX + distanceY*distanceY);
+
+            if (distance < asteroid.radius + 2) {
+                asteroid.explode();
+                this.markedForDeletion = true;
+            }
+        })
     }
     update(timeDelta) {
         this.age += timeDelta;
@@ -30,6 +41,7 @@ export default class Bullet {
         } else if (this.yPos > this.game.screenHeight + this.size / 2) {
             this.yPos = -this.size / 2;
         }
+        this.checkCollision();
     }
     draw(context) {
         context.fillRect(this.xPos, this.yPos, this.size, this.size);
