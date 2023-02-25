2fab6aee974887fbec7614c279ad6798de79669d
Sam Blundell <samcalblundell@googlemail.com>
2023-02-25 23:33:59 +0000

    add gameover text and adjust UI placement

--- UI.js
+++ UI.js
@@ -1,23 +1,29 @@
+import SpaceShip from './spaceship.js';
+
 export default class UI {
     constructor(game) {
         this.game = game;
         this.fontSize = 30;
-        this.fontFamily = 'Helvetica';
-        this.ship = 'A';
+        this.fontFamily = 'Arial';
+        this.ship = SpaceShip.DrawShip(15, 22);
         this.shipOffset = 20;
     }
 
     draw(context) {
         context.font = `${this.fontSize}px ${this.fontFamily}`;
-        context.textAlign = 'left';
+        // context.textAlign = 'left';
         context.fillStyle = this.game.fontColor;
         // temp lives display
         for (let i = 0; i < this.game.lives; i += 1) {
-            context.fillText('A ', this.shipOffset, 50);
-            this.shipOffset += 25;
+            context.drawImage(this.ship, this.shipOffset, 50);
+            this.shipOffset += 20;
         }
         this.shipOffset = 20;
         // score
-        context.fillText(`${this.game.score}`, this.game.screenWidth / 2, 50);
+        context.fillText(`${this.game.score}`, 20, 40);
+        if (this.game.lives < 1) {
+            context.font = `60px ${this.fontFamily}`;
+            context.fillText('GAME OVER', 310, this.game.screenHeight / 2);
+        }
     }
 }
